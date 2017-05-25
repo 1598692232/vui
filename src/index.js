ba9f6c7902a161f9140a4b46ba0ee1e18a37f3b6
彭志亮 <EX-pengzhiliang001@pingan.com.cn>
2017-05-25 13:41:46 +0800

    tijiao

---
+++ src/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by EX-pengzhiliang001 on 2017-05-25.
+ */
---
+++ src/vui.js
@@ -0,0 +1,15 @@
+/**
+ * Created by EX-pengzhiliang001 on 2017-05-25.
+ */
+
+import vue from 'vue'
+import VueScroller from 'vue-scroller'
+Vue.use(VueScroller)
+
+import Swiper from './components/swiper'
+
+Vue.component('swiper', Swiper)
+
+export default {
+    Swiper
+}
