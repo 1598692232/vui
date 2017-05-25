70bd5f36dfdaa2aa92f69115129c6391e75a1caa
彭志亮 <EX-pengzhiliang001@pingan.com.cn>
2017-05-25 14:04:58 +0800

    vui初步编译

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 /**
  * Created by EX-pengzhiliang001 on 2017-05-25.
  */
+module.exports = require('./vui')
--- src/vui.js
+++ src/vui.js
@@ -2,14 +2,35 @@
  * Created by EX-pengzhiliang001 on 2017-05-25.
  */
 
-import vue from 'vue'
+import Vue from 'vue'
 import VueScroller from 'vue-scroller'
+import Swiper from './components/swiper'
+import Backdesk from './components/backdesk'
+import Actions from './components/actions'
+import Datepicker from './components/datepicker'
+import Diabox from './components/Diabox'
+import IosSelect from './components/iosslect'
+import Ranger from './components/ranger'
+import Scroll from './components/scroll'
+
 Vue.use(VueScroller)
 
-import Swiper from './components/swiper'
-
 Vue.component('swiper', Swiper)
+Vue.component('Backdesk', Backdesk)
+Vue.component('Actions', Actions)
+Vue.component('Datepicker', Datepicker)
+Vue.component('Diabox', Diabox)
+Vue.component('IosSelect', IosSelect)
+Vue.component('Ranger', Ranger)
+Vue.component('Scroll', Scroll)
 
 export default {
-    Swiper
+    Swiper,
+    Backdesk,
+    Actions,
+    Datepicker,
+    Diabox,
+    IosSelect,
+    Ranger,
+    Scroll
 }
