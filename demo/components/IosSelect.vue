8ff7ae5793a975701aa3f00bf3fe5bbbd460bfa9
1037359589 <1037359589@qq.com>
2017-07-12 14:41:08 +0800

    tijiao

--- demo/components/IosSelect.vue
+++ demo/components/IosSelect.vue
@@ -11,7 +11,6 @@
           <div style="margin-bottom:30px;">
             <ios-select name="" id="" :select-list="selectAll2" ref="iosselect1" @loop:event="getValue" :initVal="[4,5,6,7]"></ios-select>
           </div>
-          asdasdasdasd
         </div>
     </div>
 
